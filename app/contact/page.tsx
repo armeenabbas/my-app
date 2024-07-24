830afba3657d9b41af83b128a5013e511487d808
armeenabbas <armeen.abbasali@gmail.com>
2024-07-25 01:12:07 +0500

    Linksand gobal css changes

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+function contact(){
+    return(
+        <div>
+        <h2 className="contact">contact</h2>
+        <p>this is contact</p>
+    
+        </div>
+      )
+    }
+    export default contact;
